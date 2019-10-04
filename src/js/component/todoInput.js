c99bc6119f9ac758fc78f7cf0ae740d98472fd60
rolobergmann <48064757+rolobergmann@users.noreply.github.com>
2019-10-03 21:22:31 -0300

    addItem listo

--- src/js/component/todoInput.js
+++ src/js/component/todoInput.js
@@ -4,18 +4,17 @@ import "./todoInput.css";
 export default class TodoInput extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: "test", list: [] };
+		this.state = { value: "", list: [] };
 
 		this.addItem = this.addItem.bind(this);
 	}
 
-	addItem(value) {
-		console.log(value);
-	}
-
-	handlerSubmit(e) {
-		e.preventDefault();
-		console.log(e);
+	addItem(todo) {
+		todo.preventDefault();
+		let a = <li className="list-group-item">{this.state.value}</li>;
+		let b = this.state.list;
+		b = b.concat(a);
+		this.setState({ list: b });
 	}
 
 	render() {
@@ -23,23 +22,20 @@ export default class TodoInput extends React.Component {
 			<div>
 				<ul className="list-group">
 					<li className="list-group-item">
-						<div className="form-group">
-							<br />
-							<form onSubmit={this.handlerSubmit}>
-								<input
-									type="text"
-									className="form-control-plaintext"
-									placeholder="What needs to be done?"
-									onChange={e =>
-										this.setState({ value: e.target.value })
-									}
-								/>
-							</form>
-						</div>
+						<br />
+						<form onSubmit={this.addItem}>
+							<input
+								type="text"
+								className="form-control-plaintext"
+								placeholder="What needs to be done?"
+								onChange={e =>
+									this.setState({ value: e.target.value })
+								}
+							/>
+						</form>
 					</li>
-					<li className="list-group-item">primer elemento</li>
-					<li className="list-group-item">{this.state.list}</li>
-					<li className="list-group-item">{this.state.list}</li>
+
+					{this.state.list}
 				</ul>
 				{this.state.value}
 			</div>
