e1f0bdf95df7c6adde1ef26a792459252c63a391
rolobergmann <rolando.bergmann.p@gmail.com>
2019-10-07 23:59:50 -0300

    Octubre07

--- src/js/component/todoInput.js
+++ src/js/component/todoInput.js
@@ -4,25 +4,44 @@ import "./todoInput.css";
 export default class TodoInput extends React.Component {
 	constructor(props) {
 		super(props);
+		this.itemInput = React.createRef();
 		this.state = {
 			value: "",
-			list: ["holasdfasd"]
+			list: [
+				{ id: 0, text: "Make dinner tonight!" },
+				{ id: 1, text: "Fold the laundry." },
+				{ id: 2, text: "Learn to make a React app!" }
+			],
+			nextId: 3,
+			isHovering: false
 		};
-
+		this.mhandleMouseHover = this.handleMouseHover.bind(this);
 		this.addItem = this.addItem.bind(this);
 		this.removeItem = this.removeItem.bind(this);
 		this.addValue = this.addValue.bind(this);
+		this.toggleHoverState = this.toggleHoverState.bind(this);
 	}
 
-	addItem(value) {
-		value.preventDefault();
+	handleMouseHover() {
+		this.setState(this.toggleHoverState);
+	}
+
+	toggleHoverState(state) {
+		return {
+			isHovering: !state.isHovering
+		};
+	}
+
+	addItem(newText) {
+		newText.preventDefault();
 		let b = this.state.list.slice();
 		console.log(b);
-		b.push(this.state.value);
-		console.log(b);
+		b.push({ id: this.state.nextId, text: this.itemInput.current.value });
 		this.setState({
-			list: b
+			list: b,
+			nextId: b.length + 1
 		});
+		this.itemInput.current.value = "";
 	}
 
 	addValue = event => {
@@ -32,7 +51,7 @@ export default class TodoInput extends React.Component {
 	};
 
 	removeItem(id) {
-		console.log("hello");
+		console.log("hello", id);
 
 		this.setState({
 			list: this.state.list.filter((item, index) => item.id !== id)
@@ -51,14 +70,31 @@ export default class TodoInput extends React.Component {
 								className="form-control-plaintext"
 								placeholder="What needs to be done?"
 								onChange={this.addValue}
+								ref={this.itemInput}
 							/>
 						</form>
 					</li>
-					{this.state.list}
-					{this.state.list.map(a => {
-						<div>
-							<li>{a}</li>
-						</div>;
+					{this.state.list.map(item => {
+						return (
+							<li
+								className="list-group-item"
+								key={item.id}
+								onMouseEnter={this.handleMouseHover}
+								onMouseLeave={this.handleMouseHover}
+								id={item.id}>
+								{item.text}
+
+								{this.state.isHovering && (
+									<a
+										href="#"
+										onClick={() =>
+											this.removeItem(item.id)
+										}>
+										X
+									</a>
+								)}
+							</li>
+						);
 					})}
 				</ul>
 			</div>
